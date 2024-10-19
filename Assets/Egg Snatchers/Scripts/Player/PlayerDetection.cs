b3522210cc203ae42842c18ff82ce520f4691338
Kubilay Teker <yusuf-kubi@hotmail.com>
2024-10-19 16:44:10 +0300

    21.Adding the egg

--- Assets/Egg Snatchers/Scripts/Player/PlayerDetection.cs
+++ Assets/Egg Snatchers/Scripts/Player/PlayerDetection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
@@ -11,8 +12,11 @@ public class PlayerDetection : MonoBehaviour
     [Header(" Elements ")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask trampolineMask;
+    [SerializeField] private LayerMask eggMask;
     [SerializeField] private BoxCollider boxCollider;
     [SerializeField] private CapsuleCollider capsuleCollider;
+
+    public bool IsHoldingEgg { get; private set; }
 
     void Start()
     {
@@ -23,21 +27,11 @@ public class PlayerDetection : MonoBehaviour
     void Update()
     {
         DetectTrampolines();
+
+        if (!IsHoldingEgg)
+            DetectEggs();
     }
 
-    /*
-    public bool CanGoThere(Vector3 targetPosition)
-    {
-        Vector3 center = targetPosition + boxCollider.center;
-        Collider[] colliders = Physics.OverlapBox(center, boxCollider.bounds.extents / 2, Quaternion.identity, groundMask);
-
-        foreach (Collider col in colliders)
-            Debug.Log(col.name);
-
-        return colliders.Length <= 0;
-
-        //return true;
-    }*/
 
     private void DetectTrampolines()
     {
@@ -48,23 +42,51 @@ public class PlayerDetection : MonoBehaviour
             playerController.Jump();
     }
 
+    private void DetectEggs()
+    {
+        Collider[] detectedEggs = DetectColliders(transform.position, eggMask, out Collider egg);
+
+        if (egg == null)
+            return;
+
+        GrabEgg(egg);
+
+        Debug.Log("Egg Detected");
+    }
+
+    private void GrabEgg(Collider egg)
+    {
+        egg.transform.SetParent(transform);
+        egg.transform.localPosition = Vector3.up * 3.5f;
+
+        IsHoldingEgg = true;
+    }
+
     public bool CanGoThere(Vector3 targetPosition, out Collider firstCollider)
     {
-        Vector3 center = targetPosition + capsuleCollider.center;
+        Collider[] detectedColliders = DetectColliders(targetPosition, groundMask, out firstCollider);
+        return detectedColliders.Length <= 0;
+
+        //return Physics.OverlapCapsule(point0, point1, capsuleCollider.radius, groundMask).Length <= 0;
+    }
+
+    private Collider[] DetectColliders(Vector3 position, LayerMask mask, out Collider firstCollider)
+    {
+        Vector3 center = position + capsuleCollider.center;
 
         float halfHeight = (capsuleCollider.height / 2) - capsuleCollider.radius;
         Vector3 offset = transform.up * halfHeight;
         Vector3 point0 = center + offset;
         Vector3 point1 = center - offset;
 
-        Collider[] colliders = Physics.OverlapCapsule(point0, point1, capsuleCollider.radius, groundMask);
+        Collider[] colliders = Physics.OverlapCapsule(point0, point1, capsuleCollider.radius, mask);
 
         if (colliders.Length > 0)
             firstCollider = colliders[0];
         else
             firstCollider = null;
 
-        return Physics.OverlapCapsule(point0, point1, capsuleCollider.radius, groundMask).Length <= 0;
+        return colliders;
     }
 
     public bool IsGrounded()
