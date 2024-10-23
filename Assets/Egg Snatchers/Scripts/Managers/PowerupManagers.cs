3079d7e76faa5cb9862c2ab2ff4f701e8ee66126
Kubilay Teker <yusuf-kubi@hotmail.com>
2024-10-23 17:46:55 +0300

    32.Spawing the Powerup

---
+++ Assets/Egg Snatchers/Scripts/Managers/PowerupManagers.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PowerupManagers : NetworkBehaviour, IGameStateListener
+{
+    [Header("value")]
+    [SerializeField] private Powerup[] powerups;
+    [SerializeField] private Transform[] spawnPositions;
+
+    public void GameStateChangedCallback(GameState gameState)
+    {
+        if (!IsServer)
+            return;
+
+        if (gameState == GameState.Game)
+            StartSpawingPowerups();
+        else
+            StopSpawningPowerups();
+    }
+    private void StartSpawingPowerups()
+    {
+        StartCoroutine(SpawnPowerupsCoroutine());
+    }
+
+    IEnumerator SpawnPowerupsCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        SpawnPowerup();
+
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(5);
+            SpawnPowerup();
+        }
+    }
+
+    private void SpawnPowerup()
+    {
+        if (transform.childCount > 0)
+            return;
+
+        Powerup randomPowerup = powerups[Random.Range(0, powerups.Length)];
+        Transform randomPosition = spawnPositions[Random.Range(0, spawnPositions.Length)];
+
+        Powerup powerupInstance = Instantiate(randomPowerup, randomPosition.position, Quaternion.identity, transform);
+        powerupInstance.GetComponent<NetworkObject>().Spawn();
+        powerupInstance.transform.SetParent(transform);
+    }
+
+    private void StopSpawningPowerups()
+    {
+        StopAllCoroutines();
+    }
+}
